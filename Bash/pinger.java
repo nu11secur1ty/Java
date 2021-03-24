d3063221c98fc8636543b25fd63f00062f8a0182
nu11secur1ty <venvaropt@gmail.com>
2021-03-24 11:36:27 +0200

    Update pinger.java

--- Bash/pinger.java
+++ Bash/pinger.java
@@ -3,34 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class pinger {
-
-    public static void main(String[] args) {
-
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        // Windows
-        processBuilder.command("cmd.exe", "/c", "ping -n 3 google.com");
-
-        try {
-
-            Process process = processBuilder.start();
-
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = process.waitFor();
-            System.out.println("\nExited with error code : " + exitCode);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
+public static void main(String[] args) {
+       	Process p;
+	try {
+		String[] cmd = { "sh", "pinger.sh"};
+   		p = Runtime.getRuntime().exec(cmd); 
+		p.waitFor(); 
+		BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
+		
+		String line; 
+		while((line = reader.readLine()) != null) { 
+			System.out.println(line);
+		} 
+	
+	} catch (IOException e) {
+   // TODO Auto-generated catch block
+   
+		e.printStackTrace();
+	
+	} catch (InterruptedException e) {
+   // TODO Auto-generated catch block
+   
+	  e.printStackTrace();
+   	}
+  }
 }
