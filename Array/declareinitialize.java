7c975347d47d83f014b4d2a8a9d165da10c80f6c
nu11secur1ty <venvaropt@gmail.com>
2021-03-24 12:27:54 +0200

    Update declareinitialize.java

--- Array/declareinitialize.java
+++ Array/declareinitialize.java
@@ -2,9 +2,10 @@
 //and initialization of Java array in a single line  
 class declareinitialize{  
 public static void main(String args[]){  
+  
   int a[]={33,3,4,5};//declaration, instantiation and initialization  
-  //printing array  
-  for(int i=0;i<a.length;i++)//length is the property of array  
-  System.out.println(a[i]);  
+    //printing array  
+    for(int i=0;i<a.length;i++)//length is the property of array  
+    System.out.println(a[i]);  
   }
 }
