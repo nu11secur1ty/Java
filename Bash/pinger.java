b37c2fed0627d3a7dcfb74ff8ea8fe4646bcb047
nu11secur1ty <venvaropt@gmail.com>
2021-03-24 11:00:22 +0200

    Update pinger.java

--- Bash/pinger.java
+++ Bash/pinger.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ProcessBuilderExample1 {
+public class pinger {
 
     public static void main(String[] args) {
 
